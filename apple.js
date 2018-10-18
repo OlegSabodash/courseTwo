213c3e8f1aba8bf49382824dcbeb839fef9cac5f
OlegSabodash <o.sabodash@mail.ru>
2018-10-18 22:49:56 +0300

    My first commit

--- apple.js
+++
@@ -1,7 +0,0 @@
-function Apple(parOne, parTwo, callback) {
-    this.name = parOne;
-    this.isWinter = parTwo;
-    this.isDirty = true;
-    this.isRotten = false;
-    this.rottedCallback = callback;
-}
